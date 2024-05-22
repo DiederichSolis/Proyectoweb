0c641be11a4f284b3516593f8ed98f98540fdba8
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-21 23:04:40 -0600

    feat: componentes de view

---
+++ WEBCHAMPIONS/src/components/View.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const View = () => {
+    return (
+        <main>
+            <img src="https://img.fcbayern.com/image/upload/t_cms-4x3-seo/v1604104556/cms/public/images/fcbayern-com/homepage/club/erfolge/champions-league/2020_header.jpg" alt="view" width="100%" height="100%" />
+            <img src="https://cloudfront-us-east-2.images.arcpublishing.com/reuters/GVKRY6LADFJUDB75BOE55MDS44.jpg" alt="view" width="100%" height="100%" />
+            <img src="https://img.olympics.com/images/image/private/t_s_pog_staticContent_hero_xl_2x/f_auto/primary/lxe9mu744i9mxuzt1b36" alt="view" width="100%" height="100%" />
+            <img src="https://assets.goal.com/images/v3/blt146f2815f515a556/GettyImages-2147034939.jpg?auto=webp&format=pjpg&width=3840&quality=60" alt="view" width="100%" height="100%" />
+        </main>
+    );
+}
+
+export default View;

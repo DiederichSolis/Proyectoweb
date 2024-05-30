a2c8cf9e702f62995b3d7b0225c5954eac795443
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-29 22:25:18 -0600

    feaT: modificaciones

--- WEBCHAMPIONS/src/principal.jsx
+++ WEBCHAMPIONS/src/principal.jsx
@@ -1,114 +1,130 @@
 import React from "react"
 import { Link } from 'react-router-dom'
 
-const NavBar1 = () => {
+const Navbar1 = () =>{
+    const navbar = {
+        position: 'fixed',
+        left: '27%',
+            top: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '10vh',
+        width: '50%',
+        padding: '1rem',
+        alignItems: 'center', // Centrar horizontalmente los elementos
+        background: 'rgba(0, 0, 0, 0.5)', // Color negro transparente
+        color: '#fff',
+        borderRadius: '10px',
+      
+        zIndex: 1000, // Asegurar que esté por encima de otros elementos
+      
+      };
+      
+      
+      const navbarLinks = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+        margin: '0 20%',
+        gap: '30%', // Espacio entre los enlaces
+        
+      };
+      
+      const navbarLink = {
+          color: '#fff',
+          fontFamily: 'Century Gothic, sans-serif',
+          fontSize: '2rem', // Tamaño de fuente más grande
+          marginBottom: '2rem', // Mayor separación entre enlaces
+          fontWeight: 'bold',
+      };
+      
+      
+        
+      
+      const iconStyles = {
+        color: 'white',
+        marginLeft: '10px',
+        marginRight: '10px',
+        textDecoration: 'none',
+        fontSize: '1.5em'
+      };
+      
+        return (
+          <nav style={navbar}>
+            <div style={navbarLinks}>
+              <a href='/' style={navbarLink}>HOME</a>
+              <a href='/champions'  style={navbarLink}>CHAMPIONS</a>
+              <a href='/partidos'  style={navbarLink}>SEMIFINAL</a>
+            </div>
+          </nav>
+        );
+      };
+  
+
+const NavBar = () => {
     const navbar = {
       position: 'fixed',
       left: '50px',
-      bottom: 'px',
+          bottom: '27%',
       display: 'flex',
       flexDirection: 'column',
-      height: '50vh',
-      width: '200px',
-      justifyContent: 'center', // Centrar verticalmente los elementos
+      height: '40vh',
+          width: '200px',
       padding: '1rem',
-      background: ' rgba(2, 48, 71, 1)',
+      justifyContent: 'center', // Centrar verticalmente los elementos
+      alignItems: 'center', // Centrar horizontalmente los elementos
+      background: 'rgba(0, 0, 0, 0.5)', // Color negro transparente
       color: '#fff',
       borderRadius: '10px',
-      zIndex: 1000,
+    
+      zIndex: 1000, // Asegurar que esté por encima de otros elementos
+    
     };
-  
+    
+    
     const navbarLinks = {
       listStyle: 'none',
-      padding: 0,
+      justifyContent: 'center', // Centrar verticalmente los elementos
+      alignItems: 'center', // Centrar horizontalmente los elementos
+      padding: 0
+      
     };
-  
+    
     const navbarLink = {
-      color: '#fff',
-      fontFamily: 'Century Gothic, sans-serif',
-      fontSize: '2rem',
-      marginBottom: '2rem', // Aumento del margen inferior para más espacio
-      fontWeight: 'bold',
+        color: '#fff',
+        fontFamily: 'Century Gothic, sans-serif',
+        fontSize: '2rem', // Tamaño de fuente más grande
+        marginBottom: '2rem', // Mayor separación entre enlaces
+        fontWeight: 'bold',
     };
-  
+    
+    
+      
+    
     const iconStyles = {
       color: 'white',
       marginLeft: '10px',
       marginRight: '10px',
       textDecoration: 'none',
-      fontSize: '1.5em',
-    };
-  
-    return (
-      <nav style={navbar}>
-        <div>
-          <a href="https://www.instagram.com/championsleague" style={iconStyles}>
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="https://www.facebook.com/uefachampionsleague" style={iconStyles}>
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="https://www.youtube.com/channel/UCpcTrCXblq78GZrTUTLWeBw" style={iconStyles}>
-            <i className="fab fa-youtube"></i>
-          </a>
-        </div>
-      </nav>
-    );
-  };
-
-const NavBar = () => {
-    const styles = {
-        position: 'fixed', // Fijar la posición de la barra de navegación
-        left: 0, // Colocarla en la parte superior
-        zIndex: 1000, // Asegurar que esté por encima de otros elementos
-        background: 'linear-gradient(to bottom, rgba(10, 10, 10, 1), rgba(10, 10, 10, 0))', // color de fondo
-        color: 'black', // color de texto
-        display: 'flex',
-        justifyContent: 'center', // Centrar verticalmente los elementos
-        alignItems: 'center', // Centrar horizontalmente los elementos
-        justifyContent: 'space-between',
-        padding: '0%',
-        alignItems: 'center',
-        height: '15vh',
-        width: '100%',
-    };
-
-    const logo = {
-        filter: 'invert(100%)',
-        padding: '5%',
-        height: '65%',
-        width: '7%',
-    };
-
-    const titulos = {
-        fontFamily: 'Century Gothic, sans-serif',
-        fontSize: '1.5em', 
-        fontWeight: 'bold',
-        margin: '0',
-        padding: '9%',
-        color: 'white',
-        textDecoration: 'none', 
-    };
-
-    const titulos1 = {
-        fontFamily: 'Century Gothic, sans-serif',
-        fontSize: '1.5em', 
-        fontWeight: 'bold',
-        margin: '0',
-        padding: '15%',
-        color: 'white',
-        textDecoration: 'none', 
+      fontSize: '1.5em'
     };
     
-    return (
-        <nav style={styles}>
-            <img src='public/champions.png' alt="logo" width="40" height="40" style={logo} />
-            <Link to='/' style={titulos}>HOME</Link>
-            <Link to='/champions' style={titulos}>CHAMPIONS</Link>
-            <Link to='/partidos' style={titulos1}>SEMIFINAL</Link>
+      return (
+        <nav style={navbar}>
+            <a href="https://www.instagram.com/championsleague" style={iconStyles}>
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="https://www.facebook.com/uefachampionsleague" style={iconStyles}>
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="https://www.youtube.com/channel/UCpcTrCXblq78GZrTUTLWeBw" style={iconStyles}>
+              <i className="fab fa-youtube"></i>
+            </a>
+        
         </nav>
-    );
-}
+      );
+    };
 
 const VIDEO = () => {
     return (
@@ -134,8 +150,8 @@ const Home = () => {
     };
     return (
         <main style={styles}>
+            <Navbar1 />
             <NavBar />
-            <NavBar1 />
             <VIDEO />
         </main>
     );
@@ -143,3 +159,5 @@ const Home = () => {
 
 
 export default Home;
+
+

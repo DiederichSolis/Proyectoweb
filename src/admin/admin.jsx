012be2ac6c9be0c3ef4b3374ccef1dbce3b351f3
DiederichSolis <invitado@MacBook-Air-de-Andy-2.local>
2024-05-31 01:38:33 -0600

    se agrego el update post

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -4,6 +4,135 @@ import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
 
+const UpdatePostModal = ({ show, handleClose, post, onUpdate }) => {
+    const [Ciudad, setCiudad] = useState(post.Ciudad);
+    const [Pais, setPais] = useState(post.Pais);
+    const [Equipo_local, setEquipo_local] = useState(post.Equipo_local);
+    const [Equipo_visitante, setEquipo_visitante] = useState(post.Equipo_visitante);
+    const [Goles_Local, setGoles_Local] = useState(post.Goles_Local);
+    const [Goles_Visitante, setGoles_Visitante] = useState(post.Goles_Visitante);
+    const [imagen_base64, setImagen_base64] = useState(post.imagen_base64);
+    const [Fase_champions, setFase_champions] = useState(post.Fase_champions);
+    const [Fecha_partido, setFecha_partido] = useState(post.Fecha_partido);
+
+    React.useEffect(() => {
+        setCiudad(post.Ciudad);
+        setPais(post.Pais);
+        setEquipo_local(post.Equipo_local);
+        setEquipo_visitante(post.Equipo_visitante);
+        setGoles_Local(post.Goles_Local);
+        setGoles_Visitante(post.Goles_Visitante);
+        setImagen_base64(post.imagen_base64);
+        setFase_champions(post.Fase_champions);
+        setFecha_partido(post.Fecha_partido);
+    }, [post]);
+
+    const formatDate = (date) => {
+        const d = new Date(date);
+        const year = d.getFullYear();
+        const month = String(d.getMonth() + 1).padStart(2, '0');
+        const day = String(d.getDate()).padStart(2, '0');
+        const hours = String(d.getHours()).padStart(2, '0');
+        const minutes = String(d.getMinutes()).padStart(2, '0');
+        const seconds = String(d.getSeconds()).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    };
+
+    const handleUpdatePost = async (e) => {
+        e.preventDefault();
+        try {
+            const formattedDate = formatDate(Fecha_partido);
+            const response = await fetch(`http://3.129.191.211/api/22952/posts/${post.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    Ciudad,
+                    Pais,
+                    Equipo_local,
+                    Equipo_visitante,
+                    Goles_Local,
+                    Goles_Visitante,
+                    imagen_base64,
+                    Fase_champions,
+                    Fecha_partido: formattedDate,
+                    Nombre_jugador: post.Nombre_jugador,
+                    Apellido_jugador: post.Apellido_jugador,
+                    Edad_jugador: post.Edad_jugador,
+                    Posicion_jugador: post.Posicion_jugador,
+                }),
+            });
+
+            if (response.ok) {
+                const updatedPost = await response.json();
+                onUpdate(updatedPost); // Llama a la función onUpdate para actualizar el estado en el componente padre
+                handleClose();
+            } else {
+                console.error("Error al actualizar el post", response.statusText);
+            }
+        } catch (error) {
+            console.error("Error en la red al actualizar el post", error);
+        }
+    };
+
+    return (
+        <Modal show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton>
+                <Modal.Title>Update Post</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleUpdatePost}>
+                    <Form.Group className="mb-3" controlId="Ciudad">
+                        <Form.Label>Ciudad:</Form.Label>
+                        <Form.Control type="text" value={Ciudad} onChange={(e) => setCiudad(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="Pais">
+                        <Form.Label>Pais:</Form.Label>
+                        <Form.Control type="text" value={Pais} onChange={(e) => setPais(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="Equipo_local">
+                        <Form.Label>Equipo Local:</Form.Label>
+                        <Form.Control type="text" value={Equipo_local} onChange={(e) => setEquipo_local(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="Equipo_visitante">
+                        <Form.Label>Equipo Visitante:</Form.Label>
+                        <Form.Control type="text" value={Equipo_visitante} onChange={(e) => setEquipo_visitante(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="Goles_Local">
+                        <Form.Label>Goles Local:</Form.Label>
+                        <Form.Control type="number" value={Goles_Local} onChange={(e) => setGoles_Local(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="Goles_Visitante">
+                        <Form.Label>Goles Visitante:</Form.Label>
+                        <Form.Control type="number" value={Goles_Visitante} onChange={(e) => setGoles_Visitante(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="imagen_base64">
+                        <Form.Label>Imagen (Base64):</Form.Label>
+                        <Form.Control type="text" value={imagen_base64} onChange={(e) => setImagen_base64(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="Fase_champions">
+                        <Form.Label>Fase Champions:</Form.Label>
+                        <Form.Control type="text" value={Fase_champions} onChange={(e) => setFase_champions(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="Fecha_partido">
+                        <Form.Label>Fecha Partido:</Form.Label>
+                        <Form.Control type="text" value={Fecha_partido} onChange={(e) => setFecha_partido(e.target.value)} />
+                    </Form.Group>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" type="submit">
+                            Update
+                        </Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
 const DeletePostModal = ({ show, handleClose, handleDelete, post }) => {
     return (
         <Modal show={show} onHide={handleClose} centered>
@@ -210,41 +339,39 @@ const Loading = () => {
 
 
 const PostsLoader = () => {
-    const [posts, setPosts] = React.useState([]);
-    const [loading, setLoading] = React.useState(true);
-    const [error, setError] = React.useState(null); // Estado para manejar errores
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     React.useEffect(() => {
         const fetchData = async () => {
             setLoading(true);
             try {
                 const response = await fetch('http://3.129.191.211/api/22952/posts');
-
                 if (!response.ok) {
-                    // Si la respuesta no es 2xx, considerarlo como un error
                     throw new Error(`Error: ${response.status} ${response.statusText}`);
                 }
-
                 const data = await response.json();
-
-                // Aquí puedes agregar validaciones adicionales sobre el formato de los datos si es necesario
                 if (!Array.isArray(data)) {
-                    // Suponiendo que esperas un array de posts, si no lo es, lanza un error
                     throw new Error("Formato de datos incorrecto");
                 }
-
                 setPosts(data);
             } catch (error) {
-                // Manejar cualquier error que ocurra durante la fetch o el procesamiento de los datos
-                console.error('Error al cargar los posts:', error);
                 setError(error.toString());
             } finally {
                 setLoading(false);
             }
         };
-
         fetchData();
     }, []);
+
+    const handleDeletePost = (postId) => {
+        setPosts(posts.filter(post => post.id !== postId));
+    };
+
+    const handleUpdatePost = (updatedPost) => {
+        setPosts(posts.map(post => (post.id === updatedPost.id ? updatedPost : post)));
+    };
 
     if (loading) {
         return <Loading />;
@@ -269,15 +396,16 @@ const PostsLoader = () => {
     return (
         <>
             {posts.map(post => (
-                <Card key={post.id} post={post} />
+                <Card key={post.id} post={post} onDelete={handleDeletePost} onUpdate={handleUpdatePost} />
             ))}
         </>
     );
 };
 
-const Card = ({ post }) => {
-    const [isColumnLayout, setIsColumnLayout] = React.useState(false);
+const Card = ({ post, onDelete, onUpdate }) => {
+    const [isColumnLayout, setIsColumnLayout] = useState(false);
     const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
 
     const cardStyles = {
         boxSizing: 'border-box',
@@ -288,13 +416,14 @@ const Card = ({ post }) => {
         display: 'flex',
         borderRadius: '58px',
         alignItems: isColumnLayout ? 'center' : 'center',
-        flexDirection: isColumnLayout ? 'column' : 'row', 
+        flexDirection: isColumnLayout ? 'column' : 'row',
         gap: '0.5rem',
         width: '50%', // Ajusta el ancho al 50% del contenedor padre
         height: 'auto', // Ajusta el alto automáticamente al contenido
         maxWidth: '600px', // Opcional: define un ancho máximo
         maxHeight: '400px' // Opcional: define un alto máximo
     };
+
     const cardStyle1 = {
         alignItems: 'center',
         boxSizing: 'border-box',
@@ -312,6 +441,7 @@ const Card = ({ post }) => {
         maxHeight: '400px', // Opcional: define un alto máximo
         justifyContent: 'center'
     };
+
     const containerStyles = {
         display: 'flex', // Configura el contenedor como un flexbox
         flexWrap: 'wrap', // Permite que los elementos se envuelvan si es necesario
@@ -320,8 +450,7 @@ const Card = ({ post }) => {
         alignItems: 'center', // Opcional: alinea las cards verticalmente
         marginTop: '2rem' // Espacio superior del contenedor
     };
-    
-    
+
     const TitleStyles = {
         color: 'white',
         textAlign: 'center',
@@ -347,7 +476,7 @@ const Card = ({ post }) => {
     };
 
     const handleResize = () => {
-        setIsColumnLayout(window.innerWidth <= 768); 
+        setIsColumnLayout(window.innerWidth <= 768);
     };
 
     React.useEffect(() => {
@@ -362,20 +491,18 @@ const Card = ({ post }) => {
             const response = await fetch(`http://3.129.191.211/api/22952/posts/${postId}`, {
                 method: 'DELETE',
             });
-    
+
             if (response.ok) {
                 console.log("Post eliminado con éxito");
                 onDelete(postId); // Llamar a la función onDelete pasada como prop para actualizar la lista de posts
+                setShowDeleteModal(false); // Cerrar el modal después de la eliminación
             } else {
                 console.error("Error al eliminar el post", response.statusText);
             }
         } catch (error) {
             console.error("Error en la red al eliminar el post", error);
-        } finally {
-            setShowDeleteModal(false); // Esto se ejecutará siempre, independientemente de si la eliminación fue exitosa o falló
         }
     };
-    
 
     return (
         <div style={containerStyles}>
@@ -383,15 +510,18 @@ const Card = ({ post }) => {
                 <img src={`${post.imagen_base64}`} alt={post.Equipo_local} style={imageStyles} />
             </div>
             <div style={cardStyles}>
-            <div style={{ flex: '1' }}> {/* Div para alinear texto a la derecha */}
-            <h2 style={TitleStyles}>{post.Fase_champions} </h2>
-                <h3 style={TitleStyles}>{post.Equipo_visitante + " vs " + post.Equipo_local} </h3>
-                <p style={textsStyles}>{"Goles: "+post.Goles_Visitante+" - "+post.Goles_Local}</p>
-                <p style={textsStyles}>{"Fecha: "+post.Fecha_partido}</p>
-                <p style={textsStyles}>{"Pais: "+post.Pais}</p>
-                <p style={textsStyles}>{"Cuidad: "+post.Ciudad}</p>
-            </div>
-            <Button variant="danger" onClick={() => setShowDeleteModal(true)}>
+                <div style={{ flex: '1' }}>
+                    <h2 style={TitleStyles}>{post.Fase_champions}</h2>
+                    <h3 style={TitleStyles}>{post.Equipo_visitante + " vs " + post.Equipo_local}</h3>
+                    <p style={textsStyles}>{"Goles: " + post.Goles_Visitante + " - " + post.Goles_Local}</p>
+                    <p style={textsStyles}>{"Fecha: " + post.Fecha_partido}</p>
+                    <p style={textsStyles}>{"Pais: " + post.Pais}</p>
+                    <p style={textsStyles}>{"Ciudad: " + post.Ciudad}</p>
+                </div>
+                <Button variant="primary" onClick={() => setShowUpdateModal(true)}>
+                    Update
+                </Button>
+                <Button variant="danger" onClick={() => setShowDeleteModal(true)}>
                     Delete
                 </Button>
             </div>
@@ -401,10 +531,36 @@ const Card = ({ post }) => {
                 handleDelete={handleDelete}
                 post={post}
             />
+            <UpdatePostModal
+                show={showUpdateModal}
+                handleClose={() => setShowUpdateModal(false)}
+                post={post}
+                onUpdate={onUpdate}
+            />
         </div>
-
     );
 };
+
+Card.propTypes = {
+    post: PropTypes.shape({
+        id: PropTypes.number,
+        Ciudad: PropTypes.string,
+        Pais: PropTypes.string,
+        Nombre_jugador: PropTypes.string,
+        Edad_jugador: PropTypes.number,
+        Posicion_jugador: PropTypes.string,
+        Equipo_local: PropTypes.string,
+        Equipo_visitante: PropTypes.string,
+        Goles_Local: PropTypes.number,
+        Goles_Visitante: PropTypes.number,
+        Fecha_partido: PropTypes.string,
+        imagen_base64: PropTypes.string,
+        Fase_champions: PropTypes.string,
+    }),
+    onDelete: PropTypes.func.isRequired,
+    onUpdate: PropTypes.func.isRequired,
+};
+
 
 
 
@@ -423,9 +579,11 @@ Card.propTypes = {
         Fecha_partido: PropTypes.string,
         imagen_base64: PropTypes.string,
         Fase_champions: PropTypes.string,
-
     }),
+    onDelete: PropTypes.func.isRequired,
+    onUpdate: PropTypes.func.isRequired,
 };
+
 
 const Footer = () => {
     const styles = {
